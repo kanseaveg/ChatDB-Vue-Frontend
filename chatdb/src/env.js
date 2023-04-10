44af41a6b21154e434959d3dfebbfb37f16fea39
kanseaveg <taroballscai@hotmail.com>
2023-04-10 15:11:02 +0800

    add prod env address.

--- chatdb/src/env.js
+++ chatdb/src/env.js
@@ -1,9 +1,9 @@
 // // .env.dev
-const REACT_APP_API_URL='http://10.21.76.236:8081'
+// const REACT_APP_API_URL='http://10.21.76.236:8081'
 // // .env.test
 // const REACT_APP_API_URL='http://localhost'
 // .env.prod
-// const REACT_APP_API_URL=http://localhost
+const REACT_APP_API_URL='http://47.106.212.102'
 export default REACT_APP_API_URL 
 
 
