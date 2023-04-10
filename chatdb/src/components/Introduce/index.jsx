089705785d754e627c65e7622f6f2b8b855a1191
kanseaveg <taroballscai@hotmail.com>
2023-04-10 14:50:25 +0800

    modified some example

--- chatdb/src/components/Introduce/index.jsx
+++ chatdb/src/components/Introduce/index.jsx
@@ -3,7 +3,7 @@ import './index.scss'
 import { WarningOutlined, BulbOutlined, ThunderboltOutlined } from '@ant-design/icons';
 import { Myreplace } from '../../utils/func';
 export default function Introduce({ setAddText }) {
-    const data = [{ title: 'Examples', icon: <BulbOutlined className='Introduce-main-topIcon' />, message: ['"向我展示nba里面所有球队的信息"', '"勒布朗詹姆斯打什么位置?"', '"湖人"队有哪些球员出生晚于1980年且出生于美国?'] },
+    const data = [{ title: 'Examples', icon: <BulbOutlined className='Introduce-main-topIcon' />, message: ['"向我展示nba里面所有球队的信息"', '"nba中勒布朗詹姆斯打什么位置?"', '"湖人"队有哪些球员出生晚于1980年且出生于美国?'] },
     { title: 'Capabilities', icon: <ThunderboltOutlined className='Introduce-main-topIcon' />, message: ['You can talk everything with DB', 'Allow user to provide data to query', 'Trained to decline wrong sql queries'] },
     { title: 'Limitations', icon: <WarningOutlined className='Introduce-main-topIcon' />, message: ["May occasionally generate some incorrect SQL query statement", "May occasionally query no enough data", "Limited knowledge of public db and db content"] }]
     return (
