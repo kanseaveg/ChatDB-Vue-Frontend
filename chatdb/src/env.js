5eb7211d4a8af9bc108d00fd9b4c4ff84aa21275
kanseaveg <taroballscai@hotmail.com>
2023-05-09 14:55:29 +0800

    modify test server address

--- chatdb/src/env.js
+++ chatdb/src/env.js
@@ -1,7 +1,7 @@
 // // .env.dev
-const REACT_APP_API_URL='http://10.21.76.236:8081'
+// const REACT_APP_API_URL='http://10.21.49.67:8081'
 // // .env.test
-// const REACT_APP_API_URL='http://10.21.76.236'
+const REACT_APP_API_URL='http://10.21.49.67'
 // .env.prod
 // const REACT_APP_API_URL='http://47.106.212.102'
 // const REACT_APP_API_URL='https://chat2db.com'
