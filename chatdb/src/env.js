66f94111a63fd1c9143d714c75490168c2f28733
taroballscai <taroballscai@hotmail.com>
2023-09-20 00:46:57 +0800

    changed address

--- chatdb/src/env.js
+++ chatdb/src/env.js
@@ -1,10 +1,9 @@
 // // .env.dev
-const REACT_APP_API_URL='http://10.21.49.67:8081'
+// const REACT_APP_API_URL='http://10.21.49.67:8081'
 // // .env.test
 // const REACT_APP_API_URL='http://10.21.49.67'
 // .env.prod
-// const REACT_APP_API_URL='http://47.106.212.102'
-// const REACT_APP_API_URL='https://chat2db.com'
+const REACT_APP_API_URL='https://demo.chat2db.com'
 export default REACT_APP_API_URL 
 
 
